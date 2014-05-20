25478a59db12ec735f047ee3cfc440f28320a247
shahar <shahar.b@matomy.com>
2014-05-20 16:35:21 +0300

    change to syslog again

--- src/main/java/com/mycompany/Bidder.java
+++ src/main/java/com/mycompany/Bidder.java
@@ -10,96 +10,129 @@ package com.mycompany;
 import com.ximpleware.VTDGen;
 import com.ximpleware.VTDNav;
 
+
+import org.apache.log4j.xml.DOMConfigurator;
+import org.apache.log4j.Logger;
+import org.productivity.java.syslog4j.Syslog;
+import org.productivity.java.syslog4j.SyslogIF;
 import org.vertx.java.core.AsyncResult;
 import org.vertx.java.core.AsyncResultHandler;
 import org.vertx.java.core.Handler;
 
 import org.vertx.java.core.VoidHandler;
 import org.vertx.java.core.http.*;
-import com.nesscomputing.syslog4j.*;
+
 import net.minidev.json.*;
 import org.vertx.java.core.net.NetClient;
 import org.vertx.java.core.net.NetSocket;
+import org.vertx.java.core.streams.Pump;
 import org.vertx.java.platform.Verticle;
 
 import org.vertx.java.core.buffer.Buffer;
 
 
 import java.util.Date;
+import java.util.Random;
 import java.util.UUID;
-import java.util.logging.Logger;
+
 
 
 public class Bidder extends Verticle {
 
-
+    //private static  Logger logger = org.slf4j.LoggerFactory.getLogger(Bidder.class);
     private static final String NO_BID="{\n" +
             "  \n" +
             "  \"string\": \"no bid\"\n" +
             "}";
    // private static Logger syslog = org.apache.log4j.Logger.getRootLogger();
-/*
-    private static final SyslogIF syslog = Syslog.getInstance("tcp");
+
+    private static  SyslogIF syslog = Syslog.getInstance("tcp");
+    private static  SyslogIF syslog2 = Syslog.getInstance("tcp");
+    private static  SyslogIF syslog3 = Syslog.getInstance("tcp");
+    private static SyslogIF[] syslogs =new SyslogIF[3];
         static {
 
 
 
-        syslog.getConfig().setHost("10.0.2.178");
-        syslog.getConfig().setPort(51515);
-        syslog.getConfig().setSendLocalName(false);
-        syslog.getConfig().setSendLocalTimestamp(false);
-        syslog.getConfig().setMaxMessageLength(10000);
-        syslog.getConfig().setIncludeIdentInMessageModifier(false);
-        syslog.getConfig().setUseStructuredData(false);
 
+            syslog.getConfig().setSendLocalName(false);
+            syslog.getConfig().setFacility(1);
+            syslog.getConfig().setSendLocalTimestamp(false);
+            syslog.getConfig().setHost("10.67.144.188");
+            syslog.getConfig().setPort(51515);
+
+            syslog2.getConfig().setSendLocalName(false);
+            syslog2.getConfig().setFacility(1);
+            syslog2.getConfig().setSendLocalTimestamp(false);
+            syslog2.getConfig().setHost("10.67.144.188");
+            syslog2.getConfig().setPort(51516);
+
+            syslog3.getConfig().setSendLocalName(false);
+            syslog3.getConfig().setFacility(1);
+            syslog3.getConfig().setSendLocalTimestamp(false);
+            syslog3.getConfig().setHost("10.67.144.188");
+            syslog3.getConfig().setPort(51517);
+
+            syslogs[0]=syslog;
+            syslogs[1]=syslog2;
+            syslogs[2]=syslog3;
 
     }
-*/
-   private static NetSocket socket =null;
+
 
 
     public void start() {
 
-    int inComingPort= container.config().getInteger("port");
+    final int  inComingPort= container.config().getInteger("port");
+     final int flumePort= container.config().getInteger("flume_port");
+      final String flumeHost=container.config().getString("flume_host");
+        final Random rand =new Random();
+
+
+
+
+
+
+
 
         HttpServer listen = vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
             private HttpServerRequest req;
 
 
-            public void handle(HttpServerRequest req) {
-                parse(req);
+            public void handle(final HttpServerRequest req) {
+                //    parse(req);
+
+                HttpServerRequest id = req.bodyHandler(new Handler<Buffer>() {
+                    @Override
+                    public void handle(Buffer buffer) {
+
+                        StringBuilder sb = new StringBuilder();
+                        String uuid = "{\"uuid\":\"" + UUID.randomUUID().toString() + "\"";
+                        String bidrequest = "\"bid_request\":";
+                        String date = "\"date\":\"" + new Date().toString() + "\"}";
+                        sb.append(uuid).append(",").append(bidrequest).append(buffer.toString()).append(",").append(date);
+
+
+                        syslogs[rand.nextInt(3)].debug(sb.toString().replace("\n", ""));
+
+                    }
+
+
+                });
+
                 //req.response().putHeader("Content-Length","2048");
                 req.response().setStatusCode(202);
                 req.response().end(NO_BID);
+
             }
         }).listen(inComingPort);
-        int flumePort= container.config().getInteger("flume_port");
-        String flumeHost=container.config().getString("flume_host");
-        NetClient client =vertx.createNetClient();
-        client.setReconnectAttempts(1000);
 
-        client.setReconnectInterval(500);
-        client.setSendBufferSize(4*1024);
-        client.connect(flumePort, flumeHost, new AsyncResultHandler<NetSocket>() {
-            public void handle(AsyncResult<NetSocket> asyncResult) {
-                if (asyncResult.succeeded()) {
-                    container.logger().info("We have connected! Socket is " + asyncResult.result());
-                    socket = asyncResult.result();
-                } else {
-                    asyncResult.cause().printStackTrace();
-                }
-
-                //socket.write("maybe");
-
-
-            }
-        });
 
 
     }
 
 
-
+/*
     private void parse(final HttpServerRequest req)
     {
         HttpServerRequest id = req.bodyHandler(new Handler<Buffer>() {
@@ -126,12 +159,11 @@ public class Bidder extends Verticle {
                     try {
 
                         if (!socket.writeQueueFull()) {
-                            StringBuilder sb =new StringBuilder();
-                            String uuid = "{\"uuid\":\""+ UUID.randomUUID().toString()+"\"";
-                            String bidrequest= "\"bid_request\":";
-                            String date = "\"date\":\""+ new Date().toString()+"\"}";
+                            StringBuilder sb = new StringBuilder();
+                            String uuid = "{\"uuid\":\"" + UUID.randomUUID().toString() + "\"";
+                            String bidrequest = "\"bid_request\":";
+                            String date = "\"date\":\"" + new Date().toString() + "\"}";
                             sb.append(uuid).append(",").append(bidrequest).append(buffer.toString()).append(",").append(date);
-
 
 
                             socket.write(sb.toString().replace("\n", "") + "\n");
@@ -160,12 +192,11 @@ public class Bidder extends Verticle {
 
 
                         syslog.debug(String.valueOf(jobj.get("id")));
-                        */
+
 
                     } catch (Exception e) {
                         System.out.println("exception occurred ==>" + e);
                     }
-
 
 
                 }
@@ -175,7 +206,7 @@ public class Bidder extends Verticle {
         });
 
     }
-
+  */
 
 
 
